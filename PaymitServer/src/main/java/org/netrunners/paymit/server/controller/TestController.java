9072e729053e2ffca5d9aa45c27bc0c13e1912e4
skzs <skazsi@gmail.com>
2016-03-05 00:28:11 +0100

    Introducing Spring

---
+++ PaymitServer/src/main/java/org/netrunners/paymit/server/controller/TestController.java
@@ -0,0 +1,15 @@
+package org.netrunners.paymit.server.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	@RequestMapping(value = "/test.html")
+	@ResponseBody
+	public String test() {
+		return "test";
+	}
+}
