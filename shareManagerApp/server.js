f05341da4d61db11890c120a45c0b6638dbad47f
Tamas Hoffman <tms.hoffman@gmail.com>
2016-03-05 01:05:48 +0100

    client test page created, simple msg broadcasting added

--- shareManagerApp/server.js
+++ shareManagerApp/server.js
@@ -4,15 +4,23 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 
-io.on('connect', function(socket){
-
-  console.log('client connected, socket', socket);
-
+app.get('/',function(req,res){
+     res.sendFile(__dirname + '/test.html');
 });
 
 
+io.on('connect', function(socket){
+  console.log(socket.id,'client connected, socket');
+  socket.on('disconnect', function(data){ console.log(socket.id,'client disconnected'); });
 
+  socket.on('msg', function(data){
+    console.log(socket.id,'msg arrived', data);
+    data = data || {};
+    data.id = socket.id;
+    socket.broadcast.emit('echo', data);
+  });
 
+});
 
 http.listen(3000, function(){
   console.log('shareManager listening on *:3000');
