fe83de6605ed6bcef7ee1312bba034988bd8b3ca
skzs <skazsi@gmail.com>
2016-03-05 02:50:24 +0100

    Test JSON receiving

--- PaymitServer/src/main/java/org/netrunners/paymit/server/controller/TestController.java
+++ PaymitServer/src/main/java/org/netrunners/paymit/server/controller/TestController.java
@@ -1,7 +1,9 @@
 package org.netrunners.paymit.server.controller;
 
 import org.netrunners.paymit.server.Test.Test;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,14 @@ public class TestController {
 		Test test = new Test();
 		test.setName("apple");
 		test.setPrice(14.5);
+		return test;
+	}
+
+	@RequestMapping(value = "/test2.json", method = RequestMethod.POST)
+	@ResponseBody
+	public Test test2(@RequestBody Test test) {
+		test.setName(test.getName() + "-");
+		test.setPrice(test.getPrice() + 1);
 		return test;
 	}
 }
