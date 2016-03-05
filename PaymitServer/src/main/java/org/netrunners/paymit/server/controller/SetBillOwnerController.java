e1af7cb7eb8a9c81e1fcaf8cb31a1784a9efac01
skzs <skazsi@gmail.com>
2016-03-05 03:22:57 +0100

    Logging added

--- PaymitServer/src/main/java/org/netrunners/paymit/server/controller/SetBillOwnerController.java
+++ PaymitServer/src/main/java/org/netrunners/paymit/server/controller/SetBillOwnerController.java
@@ -2,6 +2,8 @@ package org.netrunners.paymit.server.controller;
 
 import org.netrunners.paymit.server.setbillowner.SetBillOwnerRequest;
 import org.netrunners.paymit.server.setbillowner.SetBillOwnerResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SetBillOwnerController {
 
+	private Logger logger = LoggerFactory.getLogger(SetBillOwnerController.class);
+
 	@RequestMapping(value = "/setBillOwner.json", method = RequestMethod.POST)
 	@ResponseBody
 	public SetBillOwnerResponse setBillOwner(@RequestBody SetBillOwnerRequest request) {
+		logger.info("Setting owner of {} bill to {} client", request.getBillId(), request.getClientId());
 		SetBillOwnerResponse response = new SetBillOwnerResponse();
 		response.setStatus("OK");
 		return response;
