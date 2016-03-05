c78cdcbeeb39ed39f1de400191bfe65a70ce9ea7
Karoly_Nevelos <Karoly_Nevelos@epam.com>
2016-03-05 11:12:02 +0100

    Added transactionhandling

--- PaymitServer/src/main/java/org/netrunners/paymit/server/setbillowner/SetBillOwnerManager.java
+++ PaymitServer/src/main/java/org/netrunners/paymit/server/setbillowner/SetBillOwnerManager.java
@@ -1,25 +1,41 @@
 package org.netrunners.paymit.server.setbillowner;
 
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.util.Assert;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
-public class SetBillOwnerManager {
+public class SetBillOwnerManager extends JdbcDaoSupport {
 
-	private JdbcTemplate jdbcTemplate;
+	private PlatformTransactionManager transactionManager;
 
-	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
+	public void setTransactionManager(PlatformTransactionManager txManager) {
+		this.transactionManager = txManager;
 	}
 
-	public void afterPropertiesSet() throws Exception {
-		Assert.notNull(jdbcTemplate, "The jdbcTemplate cannot be null");
+	public void setBillOwner(String ownerId, String billId)
+			throws SetBillOwnerException {
+		TransactionDefinition txDef = new DefaultTransactionDefinition();
+		TransactionStatus txStatus = transactionManager.getTransaction(txDef);
+		try {
+			doSetBillOwner(ownerId, billId);
+			transactionManager.commit(txStatus);
+		} catch (SetBillOwnerException e) {
+			transactionManager.rollback(txStatus);
+			throw e;
+		}
+
 	}
 
-	public void setBillOwner(String ownerId, String billId) throws SetBillOwnerException {
-		int updated = jdbcTemplate.update("UPDATE t_bill SET ownerId = ? WHERE billId = ? AND ownerId IS NULL",
-				new Object[] { ownerId, billId });
+	public void doSetBillOwner(String ownerId, String billId)
+			throws SetBillOwnerException {
+		int updated = getJdbcTemplate()
+				.update("UPDATE t_bill SET ownerId = ? WHERE billId = ? AND ownerId IS NULL",
+						new Object[] { ownerId, billId });
 		if (updated != 1)
-			throw new SetBillOwnerException(
-					"No bill is available with " + billId + " bill identifier or it is already owned by someone");
+			throw new SetBillOwnerException("No bill is available with "
+					+ billId
+					+ " bill identifier or it is already owned by someone");
 	}
 }
