7e430a1f34b35006262dfacae801465533076c0a
Tamas Hoffman <tms.hoffman@gmail.com>
2016-03-05 14:37:04 +0100

    request added to the stack

--- shareManagerApp/server.js
+++ shareManagerApp/server.js
@@ -3,7 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 cors = require('cors');
-
+var request = require('request');
 
 app.use(cors());
 
